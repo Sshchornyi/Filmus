109ad371fba53ef046b3520d456c3ffb9a8a67ed
Oleksandr <geckblack05@gmail.com>
2024-05-23 22:26:56 +0300

    added search field functionality

---
+++ resultSearch.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var searchResults = JSON.parse(localStorage.getItem('searchResults'))
+    //const searchResults = localStorage.getItem('searchResults')
+    //console.log(searchResults)
+
+    const contentType = document.getElementById('searchResultContent-type')
+    if (searchResults && searchResults.length > 0) {
+
+        for(let i = 0; i < searchResults.length; i++){
+        const figure = document.createElement('figure')
+        console.log(searchResults[0])
+        const link = document.createElement('a')
+        link.href = 'WatchPage.html'
+
+
+        let elementData = ""
+        elementData = searchResults[i]
+        let elementDataArray = elementData.toString().split(',')
+        let name = elementDataArray[1]
+        let pathName = elementDataArray[2] +'/'+elementDataArray[0]
+        
+
+
+        const image = document.createElement('img')
+        let path = pathName + '/poster.jpg'
+        image.src = path
+
+        console.log(path)
+        
+
+        link.appendChild(image)
+
+        const caption = document.createElement('figcaption')
+        caption.textContent = name
+
+        figure.appendChild(link)
+        figure.appendChild(caption)
+
+        contentType.appendChild(figure)
+    }
+    }else{
+        const p = document.createElement('p')
+        p.textContent= 'Жодних результатів не знайдено!'
+        contentType.appendChild(p)
+    }
+    localStorage.removeItem('searchResults')
+});
---
+++ search.js
@@ -0,0 +1,65 @@
+ const series = [
+    ["Bones", "Кістки","Series"],
+    ["Constellation", "Сузір'я","Series"],
+    ["Hannibal", "Ганнібал","Series"],
+    ["LD&R", "Любов смерть і роботи","Series"],
+    ["Manhunt", "Полювання на вбивцю","Series"],
+    ["Sherlock", "Шерлок","Series"],
+    ["Supernatural", "Надприродне","Series"],
+    ["Witcher", "Відьмак","Series"]
+
+];
+const movies = [
+    ["Tusk", "Бивень", "Films"],
+    ["Menu", "Меню", "Films"],
+    ["Midsommer", "Сонцестояння", "Films"],
+    ["Pearl", "Перл", "Films"],
+    ["Sonic", "Сонік", "Films"],
+    ["LOR", "Володар кілець", "Films"],
+    ["Hustle", "Шахрайки", "Films"],
+    ["Dune", "Дюна", "Films"],
+    ["D&D", "Підземелля і дракони: Честь злодіїв", "Films"],
+    ["American Psycho", "Американський психопат", "Films"],
+];
+const cartoons = [
+    ["ATLA", "Аватар: Останній маг повітря","Cartoons"],
+    ["JackieChan", "Джекі Чан","Cartoons"],
+    ["Kung-Fu Panda", "Кунг-фу Панда","Cartoons"],
+    ["Shrek", "Шрек","Cartoons"],
+    ["TMNT", "Черепашки Мутанти Ніндзя","Cartoons"]
+]
+const anime = [
+    ["Castlevania", "Кастлванія","Anime"],
+    ["Frieren", "Фрірен","Anime"],
+    ["Hellsing", "Гелсінг: війна з поганню","Anime"],
+    ["Mashl", "Машл: Магія та М'язи","Anime"],
+    ["Naruto", "Наруто", "Anime"],
+    ["Spirited Away", "Віднесені привидами","Anime"]
+]
+
+document.getElementById('search-field').setAttribute('autocomplete', 'off');
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let searchInput = document.getElementById('search-field').value
+    searchInput = searchInput.toLowerCase().trim()
+    //let filtredSearch = Cartoons.filter(item => item.toLowerCase().includes(searchInput))
+    let filteredSearch
+    if (searchInput != null && searchInput.length > 0) {
+        let filteredAnime =  anime.filter((item) => item[1].toLowerCase().includes(searchInput))
+        let filteredCartoons = cartoons.filter((item) => item[1].toLowerCase().includes(searchInput))
+        let filteredMovies = movies.filter((item) => item[1].toLowerCase().includes(searchInput))
+        let filteredSeries = series.filter((item) => item[1].toLowerCase().includes(searchInput))
+
+
+        filteredSearch = filteredAnime.concat(filteredCartoons,filteredMovies,filteredSeries)
+
+        console.log(filteredSearch)
+
+    }
+    alert(filteredSearch.join('; '))
+    localStorage.setItem('searchResults', JSON.stringify(filteredSearch));//збереження в пам'яті
+    window.location.href = 'SearchResult.html'
+
+});
+
